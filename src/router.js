e85b6d4dddb7496178c871edcb76892d96074603
José Pereira Macedo <arseniumgx@gmail.com>
2021-09-04 16:49:44 -0300

    debug

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const moviesRoutes = require('./routes/movies.routes')
 
-router.get('/', (req, res) => res.end('root'))
+router.get('/', (req, res) => res.end(`${process.env.DB_PORT}, ${process.env.DB_URI}, ${process.env.DB_USER}, ${process.env.DB_PASS}, ${process.env.DB_NAME}`))
 router.get('/api', (req, res) => res.end('root'))
 router.use('/api/movies', moviesRoutes)
 
