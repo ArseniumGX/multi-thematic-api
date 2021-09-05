78e9c912b62baf0dd7a3274a22b6bf023533c217
José Pereira Macedo <arseniumgx@gmail.com>
2021-09-05 16:24:46 -0300

    movie controller is finished

--- src/controllers/moviesControllers.js
+++ src/controllers/moviesControllers.js
@@ -4,29 +4,26 @@ class MoviesController{
     showOne = async(req, res) => {
         const { id } = req.params
         const query = await Movie.findById(id)
-        return query ? res.status(200).json({movie: query}) 
+        return query ? res.status(200).json({response: query}) 
                      : res.status(204).json() 
     }
 
     showAll = async(req, res) => {
         const query = await Movie.find()
-        return !query.length < 1 ? res.status(200).json({movies: query}) 
-                                 : res.status(204).json()
+        return !query.length < 1 ? res.status(200).json({response: query}) 
+                                 : res.status(204).json({message: "Movie isn't registered!"})
     }
 
     create = async(req, res) => {
-        const { title, storeline, director, release, gender, imageURL, trailer } = req.body
+        const { ...params } = req.body
 
-        const movie = new Movie({
-            title,
-            storeline,
-            director,
-            release, 
-            gender,
-            imageURL,
-            trailer
-        })
+        console.log(params)
 
+        const check = await Movie.findOne({title: params.title})
+        if(check) 
+            return res.status(409).json({message: "Register already exists!", response: check})
+
+        const movie = new Movie(params)
         const query = await movie.save().then(res => res).catch(err => err)
 
         return query.errors ? res.status(406).json({message: "Some error occurred!", error: query.errors})
@@ -35,14 +32,12 @@ class MoviesController{
 
     update = async(req, res) => {
         const { id } = req.params
-        const { title, storeline, director, release, gender, imageURL, trailer } = req.body
+        const { ...params } = req.body
 
-        const query = await Movie.findByIdAndUpdate(id, )
+        const query = await Movie.findByIdAndUpdate(id, params, { new: true })
 
-        if(query === false)
-            return res.status(404).json({id, message: "Movie not found!"})
-        
-        return res.json({message: "It's ok!"})
+        return query ? res.status(202).json({message: "Movie updated!", response: query})
+                     : ses.status(204).json({message: "Movie isn't registered!"})
     }
 
     delete = async(req, res) => {
@@ -50,7 +45,7 @@ class MoviesController{
         
         const query = await Movie.findByIdAndDelete(id)
 
-        return query ? res.status(202).json({message: "Movie is deleted!", Movie: query}) 
+        return query ? res.status(202).json({message: "Movie is deleted!", response: query}) 
                      : res.status(204).json({message: "Movie isn't registered!"})
     }
 }
